e52cdd7fe296e1de9aef7a2f9a7a72c36dfc46b4
Jacob Britton <jacobbritton95@gmail.com>
2022-07-14 16:52:49 -0400

    Refactor photo functions into seperate module

--- src/home.js
+++ src/home.js
@@ -1,9 +1,10 @@
 import mainHeader from './header.js';
+import { photo, photoCredit } from './photo.js';
 import homePhoto from './images/coffee-with-joshua-mrZS86BKk0E-unsplash.jpg';
 import './styles/styles.css';
 
 
-
+/*
 function photo() {
     const container = document.createElement('div');
     container.classList.add('homepage-image');
@@ -37,14 +38,19 @@ function photoCredit() {
     
     return container;
 }
-  
+*/  
 
 function mainHome() {
     const page = document.querySelector('div#content');
 
     mainHeader();
-    page.appendChild(photo());
-    page.appendChild(photoCredit());
+    page.appendChild(photo(homePhoto));
+    page.appendChild(photoCredit(
+        'Coffee with Joshua',
+        'https://unsplash.com/@coffeewithjoshua?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+        'Unsplash', 
+        'https://unsplash.com/s/photos/doughnut-shop?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+    ));
 }
 
 
---
+++ src/photo.js
@@ -0,0 +1,47 @@
+import './styles/styles.css';
+
+
+function photo(photoSource) {
+    const container = document.createElement('div');
+    container.classList.add('homepage-image');
+
+    const photo = new Image();
+    photo.src = photoSource;
+    container.appendChild(photo);
+
+    return container;
+}
+
+
+function anchor(text, link) {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', link);
+    anchor.textContent = text;
+
+    return anchor;
+} 
+
+
+function photoCredit(
+    nameOfPhoto,
+    firstLink,
+    websiteName,
+    secondLink
+) {
+    const container = document.createElement('p');
+    const text1 = document.createTextNode('Photo by ');
+    const text2 = document.createTextNode(' on ');
+
+    container.appendChild(text1);
+    container.appendChild(anchor(nameOfPhoto, firstLink));
+    container.appendChild(text2);
+    container.appendChild(anchor(websiteName, secondLink));
+    
+    return container;
+}
+
+
+export {
+    photo,
+    photoCredit,
+};
