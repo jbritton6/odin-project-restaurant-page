82b25d362924f229150564f5163e90e3a4a7a5df
Jacob Britton <jacobbritton95@gmail.com>
2022-07-14 16:40:37 -0400

    Add photo to menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,25 @@
 import mainHeader from './header.js';
-// import homePhoto from './images/coffee-with-joshua-mrZS86BKk0E-unsplash.jpg';
+import menuPhoto from './images/lore-schodts-8BNGxSAQd6M-unsplash.jpg';
 import './styles/styles.css';
+
+
+function photo() {
+    const container = document.createElement('div');
+    container.classList.add('homepage-image');
+
+    const photo = new Image();
+    photo.src = menuPhoto;
+    container.appendChild(photo);
+
+    return container;
+}
 
 
 function mainMenu() {
     const page = document.querySelector('div#content');
 
     mainHeader();
+    page.appendChild(photo());
 }
 
 
