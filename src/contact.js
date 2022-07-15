00f8efc59ceec53e28f9b855ecd755dda777b53d
Jacob Britton <jacobbritton95@gmail.com>
2022-07-14 20:51:07 -0400

    Add basic contact page

---
+++ src/contact.js
@@ -0,0 +1,12 @@
+import mainHeader from './helper_functions/header.js';
+import './styles/styles.css';
+
+
+function mainContact() {
+    const page = document.querySelector('div#content');
+
+    mainHeader();
+}
+
+
+export default mainContact;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import mainHome from './home.js';
 import mainMenu from './menu.js';
+import mainContact from './contact.js';
 
 // mainHome();
-mainMenu();
+// mainMenu();
+mainContact();
