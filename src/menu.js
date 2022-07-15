49c8a7568d4c03647745d280d3a1d5626e2bbc6d
Jacob Britton <jacobbritton95@gmail.com>
2022-07-14 20:19:33 -0400

    Add doughnut info

--- src/menu.js
+++ src/menu.js
@@ -4,11 +4,48 @@ import menuPhoto from './images/lore-schodts-8BNGxSAQd6M-unsplash.jpg';
 import './styles/styles.css';
 
 
+function doughnutInfoRow(title, description) {
+    const rowTitle = document.createElement('p');
+    rowTitle.textContent = title;
+
+    const rowDescription = document.createElement('p');
+    rowDescription.textContent = description;
+
+    const row = document.createElement('div');
+    row.classList.add('doughnut-info-row');
+    row.appendChild(rowTitle);
+    row.appendChild(rowDescription);
+
+    return row;
+}
+
+
+function doughnutInfo() {
+    // const container = document.createElement('div');
+
+    /*
+    const itemTitle = document.createElement('p');
+    itemTitle.textContent = 'Item:';
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = 'Lorem ipsum dolor';
+    const itemContainer = document.createElement('div');
+    itemContainer.appendChild(itemTitle);
+    itemContainer.appendChild(itemDescription);
+    */
+    const doughnutInfoContainer = document.createElement('div');
+    doughnutInfoContainer.appendChild(doughnutInfoRow('Item:', 'Lorem ipsum dolor'));
+
+    // const priceContainer = document.createElement('div');
+    return doughnutInfoContainer;
+}
+
+
 function mainMenu() {
     const page = document.querySelector('div#content');
 
     mainHeader();
     page.appendChild(photo(menuPhoto));
+    page.appendChild(doughnutInfo());
     page.appendChild(photoCredit(
         'Lore Schodts',
         'https://unsplash.com/@lore_schodts?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
